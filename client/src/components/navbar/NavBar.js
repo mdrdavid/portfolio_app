e2cc1147b137362c30e4b102176e59b56037c2bb
david <davidmatovu88@gmail.com>
2022-08-20 15:05:02 +0300

    add on focus to nav links using stled componts

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,26 +1,33 @@
 import React from 'react'
 import './nav_bar.css'
 import{Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+export const NavbarLink = styled(Link)`
+ color:white;
+ font-size: 18px;
+ text-decoration: none;
+ $:hover: background-color: blue;
+ &:hover,
+&:focus{
+  color: #149ddd;
+  padding: 12px 15px;
+  margin-bottom: 8px;
+  transition: 0.3s;
+  font-size: 15px;
+};
+&:active{
+    color: white;
+};`
 
 const NavBar = () => {
   return (
     <div className='nav-container'>
-        <Link to="/" className="link" style={{ textDecoration: "none", color: "inherit" }}>
-            Home
-            </Link>
-            <Link to="/about" className="link" style={{ textDecoration: "none", color: "inherit" }}>
-            About
-            </Link>
-            <Link to="/resume" className="link" style={{ textDecoration: "none", color: "inherit" }}>
-           Resume
-            </Link>
-            <Link to="/portfolio" className="link" style={{ textDecoration: "none", color: "inherit" }}>
-            Portfolio
-            </Link>
-            <Link to="/services" className="link" style={{ textDecoration: "none", color: "inherit" }}>
-            Services
-            </Link>
-
+      <NavbarLink to="/" >Home</NavbarLink>
+      <NavbarLink to="/about" >About</NavbarLink>
+      <NavbarLink to="resume" >Resume</NavbarLink>
+      <NavbarLink to="/portfolio" >Portfolio</NavbarLink>
+      <NavbarLink to="/services" >Services</NavbarLink>
     </div>
   )
 }
