3f1bf9a41872366effd6f307389e510be130c028
david <davidmatovu88@gmail.com>
2022-08-22 10:29:50 +0300

    refactor code ;moved linkcontainer to GenerateLink

--- client/src/components/sidemenu/Menu.js
+++ client/src/components/sidemenu/Menu.js
@@ -1,30 +1,9 @@
 import React from "react";
 import "./menu.css";
-import { Sidelinks } from "../sidemenu/sideMenuIcons"
 import Footer from "../footer/Footer";
 import SocialMdeiaLinks from "../sociallinks/SocialMdeiaLinks";
 import Profile from "./Profile";
-
-
-
-const sideBarLinks = {
-  home: [{ name: "Home", icon: "home" }, { name: "About", icon: "about" ,link:"type"},
-  { name: "Resume", icon: "resume" },{ name: "Portfolio", icon: "portfolio" },{ name: "Services", icon: "services" }],
-}
-
-const generateLinkContiner = () =>{
-  const newContainer =[]
-  for(const[,value] of Object.entries(sideBarLinks)){
-  newContainer.push(
-    <div className= "link-container">
-    {value.map((link) => {
-          return <Sidelinks link={link} />;
-        })}
-    </div>
-  )
-}
-return newContainer
-}
+import GenerateLinkContainer from "./GenerateLinkContainer";
 
 
 const Menu = () => {
@@ -33,7 +12,7 @@ const Menu = () => {
       <div className="sidebar">
         <Profile name={"DAVID MATOVU"}/>
         <SocialMdeiaLinks/>
-        {generateLinkContiner()}
+        <GenerateLinkContainer/>
         <Footer/>
       </div>
     </>
