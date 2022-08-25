deab7c1aa63200eda09f4d27989758d6f9b693a1
david <davidmatovu88@gmail.com>
2022-08-25 10:54:27 +0300

    edit paragraph

--- client/src/components/aboutDavid/AboutTop.js
+++ client/src/components/aboutDavid/AboutTop.js
@@ -6,10 +6,8 @@ const AboutTop = ({title}) => {
             <h2>{title}</h2>
             <p>I am a developer enthusiast with a passion for programming.
                 ReactJS and Android development are my specialities
-                I am a developer enthusiast with a passion for programming.
-                ReactJS and Android development are my specialities
-                I am a developer enthusiast with a passion for programming.
-                ReactJS and Android development are my specialities
+                I am a hard-working, self-motivated, and responsible person with an outgoing personality and 
+                a good command of language. I enjoy meeting challenges and seeing them through while remaining confident and good-humored under pressure.
                 </p>
         </div>
     )
