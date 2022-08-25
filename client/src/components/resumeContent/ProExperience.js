04054ba24223b69aeb26ce571c2985335e867456
david <davidmatovu88@gmail.com>
2022-08-25 10:56:35 +0300

    change classes

--- client/src/components/resumeContent/ProExperience.js
+++ client/src/components/resumeContent/ProExperience.js
@@ -1,17 +1,17 @@
 
 import React from 'react'
 
-const ProExperience = ({name,title, company1, company2,date,location}) => {
+const ProExperience = ({name,title, duty1, duty2,date,location}) => {
   return (
     <div>
       <div className='name'>
-      <h4>{title}</h4>
+      <h4 className='resume-title'>{title}</h4>
       </div>
       <div className='resume-item'>
         <div className='resume-item1'>
-          <h4 className='company-name'>{company1}</h4>
+          <h4 className='company-name'>{duty1}</h4>
           <span>{date}</span>
-          <h6>{location}</h6>
+          <p><em>{location}</em></p>
           <ul>
             <li>
             Lead in the design, development, and implementation of the graphic, layout, and production communication materials
@@ -28,9 +28,9 @@ const ProExperience = ({name,title, company1, company2,date,location}) => {
           </ul>
         </div>
         <div className='resume-item2'>
-          <h3 className='company-name'>{company2}</h3>
+          <h3 className='company-name'>{duty2}</h3>
           <span>{date}</span>
-          <h6>{location}</h6>
+          <p><em>{location}</em></p>
           <ul>
             <li>
             Lead in the design, development, and implementation of the graphic, layout, and production communication materials
